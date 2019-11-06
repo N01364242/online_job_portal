e6f95e11aaa0acefbbe221f0c526198e04e38865
Toralpansuria <44584836+Toralpansuria@users.noreply.github.com>
2019-11-05 23:57:30 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<footer>
-    <hr>
-    <p>STAY IN TOUCH</p>
-    <div class="end">
-        <a href="https://facebook.com"><i class="fa fa-facebook"></i></a>
-        <a href="https://twitter.com"><i class="fa fa-twitter"></i></a>
-        <a href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
-        <a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i></a>
+<!-- footer -->
+<footer  style="background-color: #2c292f">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-4 text-center text-md-left ">
+
+                <div class="py-0">
+                    <h3 class="my-4 text-white">About<span class="mx-2 font-italic text-warning ">JobStock</span></h3>
+
+                    <p class="footer-links font-weight-bold">
+                        <a class="text-white" href="#">Home</a>
+                        |
+                        <a class="text-white" href="#">Blog</a>
+                        |
+                        <a class="text-white" href="#">About</a>
+                        |
+                        <a class="text-white" href="#">Contact</a>
+                    </p>
+                    <p class="text-light py-2">&copy;2019 JobStock humber Pvt. Ltd.</p>
+                </div>
+            </div>
+
+            <div class="col-md-4 text-white text-center text-md-left ">
+                <div class="py-2">
+                    <div>
+                            <h4 class="text-success my-4"> Contact Information</h4>
+                    </div>
+
+                    <div>
+                        <p><i class="fa fa-phone  mx-2 "></i> +91 12-20772532</p>
+                    </div>
+                    <div>
+                        <p><i class="fa fa-envelope  mx-2"></i><a href="mailto:support@jobstock.com">support@jobstock.com</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 text-white my-4 text-center text-md-left ">
+                <h4><span class=" font-weight-bold ">Follow Us</span></h4>
+
+                <div class="py-2">
+                    <a href="https://facebook.com"><i class="fa fa-facebook">
+                        </i><span class="mx-2 font-italic text-danger "> Facebook</span></a>
+                </div>
+                <div class="py-2">
+                    <a href="https://twitter.com"><i class="fa fa-twitter"></i>
+                        <span class="mx-2 font-italic text-danger "> Twitter</span></a>
+                </div>
+                <div class="py-2">
+                    <a href="https://www.instagram.com"><i class="fa fa-instagram"></i>
+                        <span class="mx-2 font-italic text-danger "> Instagram</span></a>
+                </div>
+                <div class="py-2">
+                    <a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i>
+                        <span class="mx-2 font-italic text-danger "> LinkedIn</span></a>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
-</body>
-</html>
